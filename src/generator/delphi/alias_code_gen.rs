bdf0d5235361340b4500b14ba555bfcbcecc2f11
bierbaumtim <tb2706@outlook.de>
2023-08-02 19:37:36 +0200

    add first tests for alias_code_gen, fix some issues

--- src/generator/delphi/alias_code_gen.rs
+++ src/generator/delphi/alias_code_gen.rs
@@ -7,12 +7,19 @@ use super::helper::Helper;
 pub(crate) struct TypeAliasCodeGenerator;
 
 impl TypeAliasCodeGenerator {
-    pub(crate) fn write_declarations(
-        buffer: &mut BufWriter<Box<dyn Write>>,
-        type_aliases: &Vec<TypeAlias>,
+    pub(crate) fn write_declarations<T: Write>(
+        buffer: &mut BufWriter<T>,
+        type_aliases: &[TypeAlias],
         indentation: usize,
     ) -> Result<(), std::io::Error> {
-        buffer.write_all(b"  {$REGION 'Aliases'}\n")?;
+        if type_aliases.is_empty() {
+            return Ok(());
+        }
+
+        buffer.write_fmt(format_args!(
+            "{}{{$REGION 'Aliases'}}\n",
+            " ".repeat(indentation),
+        ))?;
         for type_alias in type_aliases {
             buffer.write_fmt(format_args!(
                 "{}T{} = {};\n",
@@ -21,8 +28,100 @@ impl TypeAliasCodeGenerator {
                 Helper::get_datatype_language_representation(&type_alias.for_type),
             ))?;
         }
-        buffer.write_all(b"  {$ENDREGION}\n")?;
+        buffer.write_fmt(format_args!("{}{{$ENDREGION}}\n", " ".repeat(indentation)))?;
 
         Ok(())
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+    use std::io::BufWriter;
+
+    use crate::generator::types::DataType;
+
+    use super::*;
+
+    #[test]
+    fn write_nothing_when_no_alias_available() {
+        let type_aliases = vec![];
+        let mut buffer = BufWriter::new(Vec::new());
+        TypeAliasCodeGenerator::write_declarations(&mut buffer, &type_aliases, 0).unwrap();
+
+        let bytes = buffer.into_inner().unwrap();
+        let content = String::from_utf8(bytes).unwrap();
+
+        assert_eq!(content, "");
+    }
+
+    #[test]
+    fn write_with_0_indentation() {
+        let type_aliases = vec![TypeAlias {
+            pattern: None,
+            name: String::from("CustomString"),
+            for_type: DataType::String,
+        }];
+        let mut buffer = BufWriter::new(Vec::new());
+        TypeAliasCodeGenerator::write_declarations(&mut buffer, &type_aliases, 0).unwrap();
+
+        let bytes = buffer.into_inner().unwrap();
+        let content = String::from_utf8(bytes).unwrap();
+
+        let expected = indoc! {"
+            {$REGION 'Aliases'}
+            TCustomString = String;
+            {$ENDREGION}
+            "
+        };
+
+        assert_eq!(content, expected);
+    }
+
+    #[test]
+    fn write_with_2_indentation() {
+        let type_aliases = vec![TypeAlias {
+            pattern: None,
+            name: String::from("CustomString"),
+            for_type: DataType::String,
+        }];
+        let mut buffer = BufWriter::new(Vec::new());
+        TypeAliasCodeGenerator::write_declarations(&mut buffer, &type_aliases, 0).unwrap();
+
+        let bytes = buffer.into_inner().unwrap();
+        let content = String::from_utf8(bytes).unwrap();
+
+        let expected = indoc! {"
+              {$REGION 'Aliases'}
+              TCustomString = String;
+              {$ENDREGION}
+            "
+        };
+
+        assert_eq!(content, expected);
+    }
+
+    // #[test]
+    // fn write_all() {
+    //     let type_aliases = vec![];
+    //     let mut buffer = BufWriter::new(Vec::new());
+    //     TypeAliasCodeGenerator::write_declarations(&mut buffer, &type_aliases, 0).unwrap();
+
+    //     let bytes = buffer.into_inner().unwrap();
+    //     let content = String::from_utf8(bytes).unwrap();
+
+    //     let expected = indoc! {"
+    //         {$REGION 'Helper'}
+    //         function BinToHexStr(const pBin: TBytes): String;
+    //         begin
+    //           var vTemp: TBytes;
+    //           BinToHex(pBin, 0, vTemp, Length(pBin));
+    //           Result := TEncoding.GetString(vTemp);
+    //         end;
+    //         {$ENDREGION}
+    //         \n"
+    //     };
+
+    //     assert_eq!(content, expected);
+    // }
 }
