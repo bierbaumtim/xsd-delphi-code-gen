174c9692ebebffd58930643d2a06853b51830c3c
bierbaumtim <tb2706@outlook.de>
2023-08-26 14:26:55 +0200

    generate windows res

---
+++ build.rs
@@ -0,0 +1,9 @@
+extern crate winres;
+
+fn main() {
+    if cfg!(target_os = "windows") {
+        let res = winres::WindowsResource::new();
+        // res.set_icon("test.ico");
+        res.compile().unwrap();
+    }
+}
