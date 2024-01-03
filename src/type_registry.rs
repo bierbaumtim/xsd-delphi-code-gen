cc0794e4c07390c68e3ba37c5347f3b4c395110b
Tim Bierbaum <bierbaum.tim@outlook.de>
2024-01-03 18:55:18 +0100

    more documentation

--- src/type_registry.rs
+++ src/type_registry.rs
@@ -2,6 +2,9 @@ use std::collections::HashMap;
 
 use crate::parser::types::CustomTypeDefinition;
 
+/// Stores all types that have been parsed
+/// 
+/// This is used to resolve types that are referenced by other types
 pub struct TypeRegistry {
     pub types: HashMap<String, CustomTypeDefinition>,
     gen_type_count: i64,
@@ -15,12 +18,14 @@ impl TypeRegistry {
         }
     }
 
+    /// Registers a custom type
     pub fn register_type(&mut self, custom_type: CustomTypeDefinition) {
         let name = custom_type.get_qualified_name();
 
         self.types.entry(name).or_insert(custom_type);
     }
 
+    /// Generates a unique type name for a anonymous type
     pub fn generate_type_name(&mut self) -> String {
         let name = format!("__Custom_Type_{}__", self.gen_type_count);
 
